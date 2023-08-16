17340d3ca848391253494ce549af6811a9ca38ab
porameter <paramet_su66@live.rmutl.ac.th>
2023-08-16 12:01:48 +0700

    update hw04-6

--- Homework/HW04/hw04-6-man-hour.cpp
+++ Homework/HW04/hw04-6-man-hour.cpp
@@ -33,14 +33,13 @@ void commaprint(int number) {
         printf( "%d", number) ;
         return ;
     }
-    commaprint(number / 1000) ;
+    commaprint(number / 1000);
     printf( ",%03d", number % 1000) ;
 }
 int main() {
     char id[10] ;
     float timework ;
     float money ;
-
     printf( "Test case: \n") ;
     printf( "Input the Employees ID (Max. 10 chars):\n") ;
     scanf( "%s", &id) ;
@@ -48,15 +47,13 @@ int main() {
     scanf( "%f", &timework) ;
     printf( "Salary amount/hr:\n") ;
     scanf( "%f", &money) ;
-    
-    float sum ;
+    float sum;
     sum = timework * money ;
-    
     printf( "Output: \n") ;
-    printf( "Expected Output:\nEmployees ID = %s\n", id) ;
+    printf( "Expected Output:\nEmployees ID = %s\n", id) ; 
     printf( "Salary = U$ ") ;
     commaprint((int)sum) ;
-    printf( ".%02d" , (int)(sum * 100) % 100) ;
+    printf( ".%02d", (int)(sum * 100) % 100) ;
 
     return 0 ;
-}//end main fuction
+}
