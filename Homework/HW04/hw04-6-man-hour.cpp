d8debe03e2784b50d6277b897640eab564c9036d
porameter <paramet_su66@live.rmutl.ac.th>
2023-08-16 11:59:40 +0700

    update hw04-6

--- Homework/HW04/hw04-6-man-hour.cpp
+++ Homework/HW04/hw04-6-man-hour.cpp
@@ -30,32 +30,33 @@
 #include <stdio.h>
 void commaprint(int number) {
     if (number < 1000) {
-        printf("%d", number);
-        return;
+        printf( "%d", number) ;
+        return ;
     }
-    commaprint(number / 1000);
-    printf(",%03d", number % 1000);
+    commaprint(number / 1000) ;
+    printf( ",%03d", number % 1000) ;
 }
 int main() {
-    char id[10];
-    float timework;
-    float money;
-   
-    
-    printf("Input the Employees ID (Max. 10 chars):\n");
-    scanf("%s", &id);
-    printf("Input the working hrs:\n");
-    scanf("%f", &timework);
-    printf("Salary amount/hr:\n");
-    scanf("%f", &money);
-   
-    float sum;
-    sum = timework * money;
-    
-    printf("Employees ID = %s\n", id);
-    printf("Salary = U$ ");
-    commaprint((int)sum);
-    printf(".%02d", (int)(sum * 100) % 100);
+    char id[10] ;
+    float timework ;
+    float money ;
 
-    return 0;
-}
+    printf( "Test case: \n") ;
+    printf( "Input the Employees ID (Max. 10 chars):\n") ;
+    scanf( "%s", &id) ;
+    printf( "Input the working hrs:\n") ;
+    scanf( "%f", &timework) ;
+    printf( "Salary amount/hr:\n") ;
+    scanf( "%f", &money) ;
+    
+    float sum ;
+    sum = timework * money ;
+    
+    printf( "Output: \n") ;
+    printf( "Expected Output:\nEmployees ID = %s\n", id) ;
+    printf( "Salary = U$ ") ;
+    commaprint((int)sum) ;
+    printf( ".%02d" , (int)(sum * 100) % 100) ;
+
+    return 0 ;
+}//end main fuction
